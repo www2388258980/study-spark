bd0a5e6a7973415b0a0e591ee70ac66e8685d17b
www2388258980 <2388258980@qq.com>
2021-06-09 17:08:43 +0800

    study-spark init

---
+++ spark-sql/src/main/scala/com/yj/study/spark/Test.scala
@@ -0,0 +1,18 @@
+package com.yj.study.spark
+
+import org.apache.spark.sql.SparkSession
+
+object Test {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.testing.memory", "471859200")
+      .master("local[*]")
+      .getOrCreate()
+
+    val df = spark.read.json(this.getClass.getResource("/people.json").getPath)
+    df.show()
+  }
+}
